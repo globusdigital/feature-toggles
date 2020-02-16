2487948101cec3cbbcb5b5a02c582b3bb17ab9fd
Viktor Kojouharov <developer@sugr.org>
2020-02-16 21:17:50 +0100

    more mem storage test coverage

--- storage/mem_test.go
+++ storage/mem_test.go
@@ -62,6 +62,59 @@ func TestMem_Get(t *testing.T) {
 	}
 }
 
+func TestMem_Save(t *testing.T) {
+	type args struct {
+		ctx     context.Context
+		flags   []toggle.Flag
+		initial bool
+	}
+	tests := []struct {
+		name     string
+		args     args
+		initial  []toggle.Flag
+		expected []toggle.Flag
+		wantErr  bool
+	}{
+		{name: "canceled ctx", args: args{ctx: canceledCtx()}, wantErr: true},
+		{name: "save data", args: args{ctx: context.Background(), flags: initialData}, expected: initialData},
+		{name: "save data after initial", args: args{ctx: context.Background(), flags: []toggle.Flag{
+			{Name: "n2", ServiceName: "svc1", RawValue: "1", Value: true},
+			{Name: "n3", ServiceName: "svc1", RawValue: "0"},
+		}, initial: true}, initial: initialData, expected: []toggle.Flag{
+			initialData[0], initialData[1], initialData[2], initialData[3], initialData[4],
+			{Name: "n3", ServiceName: "svc1", RawValue: "0"},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := assert.New(t)
+			s := NewMem()
+
+			if tt.initial != nil {
+				err := s.Save(context.Background(), tt.initial, false)
+				a.NoError(err)
+			}
+
+			err := s.Save(tt.args.ctx, tt.args.flags, tt.args.initial)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Mem.Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			a.Len(s.data, len(tt.expected))
+
+			for _, v := range s.data {
+				var found bool
+				for _, e := range tt.expected {
+					if e == v {
+						found = true
+					}
+				}
+				a.True(found, "expected %#v", v)
+			}
+		})
+	}
+}
+
 func canceledCtx() context.Context {
 	ctx, cancel := context.WithCancel(context.Background())
 	cancel()
