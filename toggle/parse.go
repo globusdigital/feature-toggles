70714963f55d59959381e52f5805a4e033d45333
Viktor <viktor.kojouharov@globus.ch>
2020-02-28 16:54:00 +0100

    Implement a rudimentary parser

--- toggle/parse.go
+++ toggle/parse.go
@@ -5,28 +5,29 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"strconv"
 	"unicode"
 )
 
 type kind int
 
 const (
-	ident kind = iota
+	ident kind = iota // identifier
 
-	intLit
-	floatLit
-	boolLit
-	stringLit
+	intLit    // integer
+	floatLit  // float
+	boolLit   // boolean
+	stringLit // string
 
-	andOp
-	orOp
-	eqOp
-	neOp
-	ltOp
-	gtOp
+	andOp // && operator
+	orOp  // || operator
+	eqOp  // == operator
+	neOp  // != operator
+	ltOp  // < operator
+	gtOp  // > operator
 
-	openParen
-	closeParen
+	openParen  // (
+	closeParen // )
 )
 
 type token struct {
@@ -34,6 +35,215 @@ type token struct {
 	pos    int
 	opened rune
 	val    []byte
+}
+
+func (t token) String() string {
+	return fmt.Sprintf("type %q with value %q at position %d", t.kind, string(t.val), t.pos)
+}
+
+func ParseCondition(r io.Reader) (Condition, error) {
+	tokens, err := lexer(r)
+	if err != nil {
+		return Condition{}, err
+	}
+
+	condition, pos, err := parseCondition(tokens, true, 0)
+	if err != nil {
+		return condition, err
+	}
+
+	if pos < len(tokens) {
+		return condition, fmt.Errorf("trailing tokens: %s", tokens[pos:])
+	}
+
+	if condition.Op == invalidConditionalOp {
+		condition.Op = AndOp
+	}
+
+	return condition, nil
+}
+
+func parseCondition(tokens []*token, toplevel bool, openP int) (Condition, int, error) {
+	c := Condition{Op: invalidConditionalOp}
+	i := 0
+
+	for ; i < len(tokens); i++ {
+		t := tokens[i]
+
+		switch t.kind {
+		case ident, eqOp, neOp, ltOp, gtOp, intLit, floatLit, boolLit, stringLit:
+			f, pos, err := parseField(tokens[i:])
+			if err != nil {
+				return c, i, err
+			}
+
+			i += pos
+
+			c.Fields = append(c.Fields, f)
+		case andOp, orOp:
+			op := AndOp
+			if t.kind == orOp {
+				op = OrOp
+			}
+
+			if c.Op == invalidConditionalOp || c.Op == op {
+				c.Op = op
+			} else {
+				if OrOp == op && !toplevel {
+					return c, i, nil
+				}
+
+				condition, pos, err := parseCondition(tokens[i+1:], false, openP)
+				if err != nil {
+					return c, i, err
+				}
+
+				if t.kind == andOp {
+					if len(c.Fields) > 0 {
+						// |-------------------------| parent OR condition
+						// foo == true || bar == false && alpha < 42
+						//               ^------------^ becomes part of the AND condition
+						condition.Fields = append(
+							[]ConditionField{c.Fields[len(c.Fields)-1]}, condition.Fields...)
+						if len(c.Fields) == 1 {
+							c.Fields = nil
+						} else {
+							c.Fields = c.Fields[:len(c.Fields)-1]
+
+						}
+
+						condition.Op = AndOp
+					}
+
+					if condition.Op != invalidConditionalOp {
+						c.Conditions = append(c.Conditions, condition)
+					}
+				} else {
+					if condition.Op == invalidConditionalOp {
+						// Trailing field
+						c = Condition{Op: op, Conditions: []Condition{c}, Fields: condition.Fields}
+					} else {
+						c = Condition{Op: op, Conditions: []Condition{c, condition}}
+					}
+				}
+
+				i += pos + 1
+			}
+		case openParen:
+			condition, pos, err := parseCondition(tokens[i+1:], true, openP+1)
+			if err != nil {
+				return c, i, err
+			}
+
+			c.Conditions = append(c.Conditions, condition)
+			i += pos + 1
+
+		case closeParen:
+			if openP < 1 {
+				return c, i, fmt.Errorf("unexpected token (%s)", t)
+			}
+			return c, i, nil
+		}
+	}
+
+	if openP > 0 {
+		return c, i, fmt.Errorf("imbalanced opening parenthesis")
+	}
+
+	return c, i, nil
+}
+
+func parseField(tokens []*token) (ConditionField, int, error) {
+	f := ConditionField{Op: invalidFieldOp}
+	i := 0
+
+LOOP:
+	for ; i < len(tokens); i++ {
+		t := tokens[i]
+
+		switch t.kind {
+		default:
+			i--
+			break LOOP
+		case ident:
+			// name .
+			if f.Name != "" ||
+				// value .
+				f.Name == "" && f.Value != nil && f.Op == invalidFieldOp {
+				return f, i, fmt.Errorf("unexpected token (%s)", t)
+			}
+			f.Name = string(t.val)
+		case eqOp, neOp, ltOp, gtOp:
+			// op .
+			if f.Op != invalidFieldOp ||
+				// .
+				f.Name == "" && f.Value == nil ||
+				// name value .
+				f.Name != "" && f.Value != nil {
+				return f, i, fmt.Errorf("unexpected token (%s)", t)
+			}
+
+			switch t.kind {
+			case eqOp:
+				f.Op = EqOp
+			case neOp:
+				f.Op = NeOp
+			case ltOp:
+				// Value is the first token
+				if f.Name == "" {
+					f.Op = GtOp
+				} else {
+					f.Op = LtOp
+				}
+			case gtOp:
+				// Value is the first token
+				if f.Name == "" {
+					f.Op = LtOp
+				} else {
+					f.Op = GtOp
+				}
+			}
+		case intLit, floatLit, boolLit, stringLit:
+			// value .
+			if f.Value != nil ||
+				// name .
+				f.Value == nil && f.Name != "" && f.Op == invalidFieldOp {
+				return f, i, fmt.Errorf("unexpected token (%s)", t)
+			}
+
+			var err error
+			switch t.kind {
+			case stringLit:
+				f.Value = string(t.val)
+				f.Type = StringType
+			case intLit:
+				f.Value, err = strconv.ParseInt(string(t.val), 10, 64)
+				f.Type = IntType
+			case floatLit:
+				f.Value, err = strconv.ParseFloat(string(t.val), 10)
+				f.Type = FloatType
+			case boolLit:
+				f.Type = BoolType
+				if bytes.Equal(t.val, []byte("true")) {
+					f.Value = true
+				} else if bytes.Equal(t.val, []byte("false")) {
+					f.Value = false
+				} else {
+					err = fmt.Errorf("invalid boolean value: %s", string(t.val))
+				}
+			}
+
+			if err != nil {
+				return f, i, fmt.Errorf("unexpected value for token (%s): %v", t, err)
+			}
+		}
+	}
+
+	if f.Name == "" || f.Op == invalidFieldOp || f.Value == nil {
+		return f, i, fmt.Errorf("incomplete field: %s", f)
+	}
+
+	return f, i, nil
 }
 
 func lexer(r io.Reader) ([]*token, error) {
@@ -62,7 +272,7 @@ func lexer(r io.Reader) ([]*token, error) {
 				}
 
 				if t.kind == stringLit {
-					t.val = append(t.val, []byte(string(r))...)
+					t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
 				} else {
 					t = nil
 				}
@@ -71,7 +281,7 @@ func lexer(r io.Reader) ([]*token, error) {
 			if t == nil {
 				t = &token{kind: openParen, pos: curLen}
 			} else if t.kind == stringLit {
-				t.val = append(t.val, []byte(string(r))...)
+				t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
 			} else {
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
 			}
@@ -79,7 +289,7 @@ func lexer(r io.Reader) ([]*token, error) {
 			if t == nil {
 				t = &token{kind: closeParen, pos: curLen}
 			} else if t.kind == stringLit {
-				t.val = append(t.val, []byte(string(r))...)
+				t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
 			} else {
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
 			}
@@ -87,7 +297,7 @@ func lexer(r io.Reader) ([]*token, error) {
 			if t == nil {
 				t = &token{kind: ltOp, pos: curLen}
 			} else if t.kind == stringLit {
-				t.val = append(t.val, []byte(string(r))...)
+				t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
 			} else {
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
 			}
@@ -95,44 +305,54 @@ func lexer(r io.Reader) ([]*token, error) {
 			if t == nil {
 				t = &token{kind: gtOp, pos: curLen}
 			} else if t.kind == stringLit {
-				t.val = append(t.val, []byte(string(r))...)
+				t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
 			} else {
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
 			}
 		case r == '&':
 			if t == nil {
-				t = &token{kind: andOp, pos: curLen}
-			} else if t.kind != stringLit && (t.kind != andOp || len(t.val) != 1) {
+				t = &token{kind: andOp, pos: curLen, val: []byte(string(r))}
+			} else if t.kind == stringLit {
+				t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
+			} else if t.kind != andOp || len(t.val) != 1 {
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
+			} else {
+				t.val = append(t.val, []byte(string(r))...)
 			}
-			t.val = append(t.val, []byte(string(r))...)
 		case r == '|':
 			if t == nil {
-				t = &token{kind: orOp, pos: curLen}
-			} else if t.kind != stringLit && (t.kind != orOp || len(t.val) != 1) {
+				t = &token{kind: orOp, pos: curLen, val: []byte(string(r))}
+			} else if t.kind == stringLit {
+				t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
+			} else if t.kind != orOp || len(t.val) != 1 {
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
+			} else {
+				t.val = append(t.val, []byte(string(r))...)
 			}
-			t.val = append(t.val, []byte(string(r))...)
 		case r == '!':
 			if t == nil {
-				t = &token{kind: neOp, pos: curLen}
-			} else if t.kind != stringLit {
+				t = &token{kind: neOp, pos: curLen, val: []byte(string(r))}
+			} else if t.kind == stringLit {
+				t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
+			} else {
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
 			}
-			t.val = append(t.val, []byte(string(r))...)
 		case r == '=':
 			if t == nil {
-				t = &token{kind: eqOp, pos: curLen}
-			} else if t.kind != stringLit && !((t.kind == eqOp || t.kind == neOp) && len(t.val) == 1) {
+				t = &token{kind: eqOp, pos: curLen, val: []byte(string(r))}
+			} else if t.kind == stringLit {
+				t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
+			} else if (t.kind != eqOp && t.kind != neOp) || len(t.val) != 1 {
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
+			} else {
+				t.val = append(t.val, []byte(string(r))...)
 			}
-			t.val = append(t.val, []byte(string(r))...)
 		case r == '\\':
 			if t == nil || t.kind != stringLit {
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
 			}
 			if escapeNext {
-				t.val = append(t.val, []byte(string(r))...)
+				t.val = append(t.val, '\\', '\\')
 				escapeNext = false
 			} else {
 				escapeNext = true
@@ -146,6 +366,9 @@ func lexer(r io.Reader) ([]*token, error) {
 
 			if len(t.val) > 0 {
 				if escapeNext || t.opened != r {
+					if t.opened != r && escapeNext {
+						t.val = append(t.val, '\\')
+					}
 					t.val = append(t.val, []byte(string(r))...)
 					escapeNext = false
 				} else {
@@ -158,15 +381,12 @@ func lexer(r io.Reader) ([]*token, error) {
 			}
 			switch t.kind {
 			case stringLit:
-				if escapeNext {
-					t.val = append(t.val, '\\')
-					escapeNext = false
-				}
+				t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
 			case ident:
+				t.val = append(t.val, []byte(string(r))...)
 			default:
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
 			}
-			t.val = append(t.val, []byte(string(r))...)
 		case unicode.IsNumber(r):
 			if t == nil {
 				t = &token{kind: intLit, pos: curLen}
@@ -174,15 +394,12 @@ func lexer(r io.Reader) ([]*token, error) {
 
 			switch t.kind {
 			case stringLit:
-				if escapeNext {
-					t.val = append(t.val, '\\')
-					escapeNext = false
-				}
+				t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
 			case ident, intLit, floatLit:
+				t.val = append(t.val, []byte(string(r))...)
 			default:
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
 			}
-			t.val = append(t.val, []byte(string(r))...)
 		case r == '.':
 			if t == nil {
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
@@ -191,24 +408,17 @@ func lexer(r io.Reader) ([]*token, error) {
 			switch t.kind {
 			case intLit:
 				t.kind = floatLit
+				t.val = append(t.val, []byte(string(r))...)
 			case stringLit:
-				if escapeNext {
-					t.val = append(t.val, '\\')
-					escapeNext = false
-				}
+				t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
 			default:
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
 			}
-			t.val = append(t.val, []byte(string(r))...)
 		default:
 			if t == nil || t.kind != stringLit {
 				return tokens, fmt.Errorf("invalid character %v at position %d", string(r), curLen)
 			}
-			if escapeNext {
-				t.val = append(t.val, '\\')
-				escapeNext = false
-			}
-			t.val = append(t.val, []byte(string(r))...)
+			t.val, escapeNext = addRuneToString(t.val, r, escapeNext)
 		}
 
 		if t != nil && (len(tokens) == 0 || t != tokens[len(tokens)-1]) {
@@ -233,4 +443,14 @@ func lexer(r io.Reader) ([]*token, error) {
 	}
 
 	return tokens, nil
+}
+
+func addRuneToString(buf []byte, r rune, escapeNext bool) ([]byte, bool) {
+	if escapeNext {
+		buf = append(buf, '\\')
+		escapeNext = false
+	}
+	buf = append(buf, []byte(string(r))...)
+
+	return buf, escapeNext
 }
--- toggle/parse_test.go
+++ toggle/parse_test.go
@@ -7,7 +7,43 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const cond1 = `userID < 10 && (serviceName == 'serv1' || serviceName == 'serv2') || userGroup == "tes\"t'er"`
+const (
+	cond1 = `userID < 10 && (serviceName == 'serv1' || serviceName == 'serv2') || userGroup == "tes\"t'er"`
+	cond2 = `useriD < 10 && s == true || foo == "bar" && alpha == 14 || test == 42.1 && test2 == false`
+)
+
+var (
+	cond1Exp = Condition{Op: OrOp, Conditions: []Condition{
+		{Op: AndOp,
+			Conditions: []Condition{
+				{Op: OrOp, Fields: []ConditionField{
+					{Op: EqOp, ConditionValue: ConditionValue{Name: "serviceName", Type: StringType, Value: "serv1"}},
+					{Op: EqOp, ConditionValue: ConditionValue{Name: "serviceName", Type: StringType, Value: "serv2"}},
+				}},
+			},
+			Fields: []ConditionField{
+				{Op: LtOp, ConditionValue: ConditionValue{Name: "userID", Type: IntType, Value: int64(10)}},
+			},
+		},
+	}, Fields: []ConditionField{
+		{Op: EqOp, ConditionValue: ConditionValue{Name: "userGroup", Type: StringType, Value: `tes"t'er`}},
+	}}
+
+	cond2Exp = Condition{Op: OrOp, Conditions: []Condition{
+		{Fields: []ConditionField{
+			{ConditionValue: ConditionValue{Name: "useriD", Type: IntType, Value: int64(10)}, Op: LtOp},
+			{ConditionValue: ConditionValue{Name: "s", Type: BoolType, Value: true}},
+		}},
+		{Fields: []ConditionField{
+			{ConditionValue: ConditionValue{Name: "foo", Type: StringType, Value: "bar"}},
+			{ConditionValue: ConditionValue{Name: "alpha", Type: IntType, Value: int64(14)}},
+		}},
+		{Fields: []ConditionField{
+			{ConditionValue: ConditionValue{Name: "test", Type: FloatType, Value: 42.1}},
+			{ConditionValue: ConditionValue{Name: "test2", Type: BoolType, Value: false}},
+		}},
+	}}
+)
 
 func Test_lexer(t *testing.T) {
 	tests := []struct {
@@ -23,6 +59,7 @@ func Test_lexer(t *testing.T) {
 		{name: "boolLit", in: "false", want: []*token{{kind: boolLit, pos: 0, val: []byte("false")}}},
 		{name: "stringLit", in: "'false'", want: []*token{{kind: stringLit, pos: 0, val: []byte("false"), opened: '\''}}},
 		{name: "stringLit 2", in: `"true != \"false\""`, want: []*token{{kind: stringLit, pos: 0, val: []byte("true != \"false\""), opened: '"'}}},
+		{name: "stringLit 3", in: `"some \'string"`, want: []*token{{kind: stringLit, pos: 0, val: []byte("some \\'string"), opened: '"'}}},
 		{name: "cond1", in: cond1, want: []*token{
 			{kind: ident, pos: 0, val: []byte("userID")},
 			{kind: ltOp, pos: 7},
@@ -42,6 +79,10 @@ func Test_lexer(t *testing.T) {
 			{kind: eqOp, pos: 79, val: []byte("==")},
 			{kind: stringLit, pos: 82, val: []byte(`tes"t'er`), opened: '"'},
 		}},
+		{name: "invalid float", in: "14.1.2", wantErr: true},
+		{name: "invalid char", in: "@", wantErr: true},
+		{name: ">", in: ">", want: []*token{{kind: gtOp}}},
+		{name: "complex string", in: `'some > string \< with \' \" data |= &! @% \\ ()'`, want: []*token{{kind: stringLit, val: []byte(`some > string \< with ' \" data |= &! @% \\ ()`), opened: '\''}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -51,7 +92,49 @@ func Test_lexer(t *testing.T) {
 				return
 			}
 
-			assert.Equal(t, tt.want, got)
+			if !tt.wantErr {
+				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
+
+func TestParseCondition(t *testing.T) {
+	tests := []struct {
+		name    string
+		in      string
+		want    Condition
+		wantErr bool
+	}{
+		{name: "invalid field", in: "foo true", wantErr: true},
+		{name: "invalid parent 1", in: "foo != true)", wantErr: true},
+		{name: "invalid parent 2", in: "(foo != true", wantErr: true},
+		{name: "invalid op", in: "foo != < true", wantErr: true},
+		{name: "single field", in: "foo != true", want: Condition{Fields: []ConditionField{
+			{Op: NeOp, ConditionValue: ConditionValue{Name: "foo", Type: BoolType, Value: true}},
+		}}},
+		{name: "two field 1", in: "foo != true && bar < 20", want: Condition{Fields: []ConditionField{
+			{Op: NeOp, ConditionValue: ConditionValue{Name: "foo", Type: BoolType, Value: true}},
+			{Op: LtOp, ConditionValue: ConditionValue{Name: "bar", Type: IntType, Value: int64(20)}},
+		}}},
+		{name: "two field 2", in: "foo != true || bar < 20", want: Condition{Fields: []ConditionField{
+			{Op: NeOp, ConditionValue: ConditionValue{Name: "foo", Type: BoolType, Value: true}},
+			{Op: LtOp, ConditionValue: ConditionValue{Name: "bar", Type: IntType, Value: int64(20)}},
+		}, Op: OrOp}},
+		{name: "cond1", in: cond1, want: cond1Exp},
+		{name: "cond2", in: cond2, want: cond2Exp},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseCondition(strings.NewReader(tt.in))
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseCondition() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr {
+				assert.Equal(t, tt.want, got)
+			}
 		})
 	}
 }
